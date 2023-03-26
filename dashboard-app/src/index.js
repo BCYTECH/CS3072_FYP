85b895bf5707777a8b6dca0b8c481897624271bf
Bryson C.Y <1932139@brunel.ac.uk>
2023-03-26 02:53:51 +0100

    Installed Firebase packages and init config
    - for Database Server

--- dashboard-app/src/index.js
+++ dashboard-app/src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import {initializeApp} from 'firebase/app';
 //import reportWebVitals from './reportWebVitals';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8AnbuKJEyYxvEo9t9blE5mqzlYX-WdPM",
+  authDomain: "test-470a5.firebaseapp.com",
+  databaseURL: "https://test-470a5.firebaseio.com",
+  projectId: "test-470a5",
+  storageBucket: "test-470a5.appspot.com",
+  messagingSenderId: "818708596546",
+  appId: "1:818708596546:web:48f737f284ed1693636adb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
